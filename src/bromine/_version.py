80e0a46f08af2423ed76932d12f498ae9ee0c60e
francesco tartaggia <francesco.tartaggia@etiqa.it>
2018-07-27 11:51:50 +0200

    bug fix: since raw Selenium Exceptions get raised by Bromine's WebElement when using __getattr__ to directly delegating method calls to Selenium's WebElement, trying to catch an exception wrapper just cannot work

--- src/bromine/_version.py
+++ src/bromine/_version.py
@@ -5,5 +5,5 @@ https://www.python.org/dev/peps/pep-0440/
 https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
 """
 
-__version__ = '0.1.0_BUILD'
+__version__ = '0.1.1_BUILD'
 __commit__ = 'COMMIT'
--- src/bromine/exceptions.py
+++ src/bromine/exceptions.py
@@ -2,7 +2,8 @@
 Global Bromine exception and warning classes.
 """
 
-import selenium.common.exceptions as _se
+from selenium.common.exceptions import (NoSuchElementException,
+                                        StaleElementReferenceException)
 
 
 class BromineException(Exception):
@@ -17,17 +18,9 @@ class NoSuchPageError(NoSuchObjectError):
     """Page is not registered with given application."""
 
 
-class NoSuchElementException(_se.NoSuchElementException, NoSuchObjectError):
-    """Wrapper of Selenium's NoSuchElementException."""
-
-
 class MultipleObjectsFoundError(BromineException):
     """Base error for queries returning multiple results, while only one was expected."""
 
 
 class MultipleElementsFoundError(MultipleObjectsFoundError):
     """Multiple instances of a DOM element were found, but only one was expected."""
-
-
-class StaleElementReferenceException(_se.StaleElementReferenceException, BromineException):
-    """Wrapper of Selenium's StaleElementReferenceException."""
