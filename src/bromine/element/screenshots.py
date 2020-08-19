e77108c92421a3ef2de8af3facc7d54affa5e123
Fabio Pulvirenti <fabio@etiqa.it>
2020-08-19 11:03:07 +0200

    Added Element Screenshot first implementation

--- src/bromine/element/screenshots.py
+++ src/bromine/element/screenshots.py
@@ -6,8 +6,8 @@ from bromine.utils.image import Image, ScreenshotFromImage
 
 class ElementScreenshot(object):
 
-    def __init__(self, element):
-        self._element = element
+    def __init__(self, web_element):
+        self._web_element = web_element
 
     def take(self):
         self._assert_element_is_displayed()
@@ -15,8 +15,8 @@ class ElementScreenshot(object):
 
     def _assert_element_is_displayed(self):
         assert_that(
-            self._element.is_displayed(),
-            "{element} is not displayed on browser's current window".format(element=self._element))
+            self._web_element.is_displayed(),
+            "{web_element} is not displayed on browser's current window".format(web_element=self._web_element))
 
     def _take(self):
-        raise NotImplementedError
+        return self._web_element.get_screenshot()
